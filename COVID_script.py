38223bb5b0f1fb930c23bc6c4a181b79bce5ca59
davidvs-code <dsalazar100@gmail.com>
2020-06-04 20:53:15 -0700

    Added daily cases and deaths

--- COVID_script.py
+++ COVID_script.py
@@ -1,26 +1,17 @@
 # COVID 19 Data
-# rmdir covid-19-data
 # git clone https://github.com/nytimes/covid-19-data.git
 
 import csv
 import matplotlib.pyplot as plt
 import numpy as np 
 
-
-u = []
-v = []
-w = []
-x = []
-y = []
-z = []
-
-
 filename = 'covid-19-data/us-counties.csv'
 
-
+# Extract county specific data from csv file
 san_mateo_date   = []
 san_mateo_cases  = []
 san_mateo_deaths = []
+
 with open(filename, 'r') as csvfile:
 	counties = csv.DictReader(csvfile, delimiter=',')
 	for row in counties:
@@ -35,30 +26,44 @@ sm_cases  = list(san_mateo_cases)
 sm_deaths = list(san_mateo_deaths)
 
 
-# Plot raw signals
+# Calculate the diffirence in cases and deaths
+cases_diff  = []
+deaths_diff = []
+counter = 0
 
+for num in range(len(san_mateo_cases)-1):
+	cases_diff.append([float(san_mateo_cases[num+1]) - float(san_mateo_cases[num])])
+	deaths_diff.append([float(san_mateo_deaths[num+1]) - float(san_mateo_deaths[num])])
+
+
+#### Create figures
+
+# Plot raw signals
 # plt.figure(1)
 # plt.plot(sm_cases)
 # plt.xlabel('Date')
-# plt.ylabel('Number')
+# plt.ylabel('Cummulative Cases')
 # #plt.legend()
 # plt.show()	
 
-plt.figure(2)
-plt.plot(sm_deaths)
-plt.xlabel('Date')
-plt.ylabel('Number')
-#plt.legend()
-plt.show()	
+# plt.figure(2)
+# plt.plot(sm_deaths)
+# plt.xlabel('Date')
+# plt.ylabel('Cummulative Deaths')
+# #plt.legend()
+# plt.show()	
 
+# plt.figure(3)
+# plt.plot(cases_diff)
+# plt.xlabel('Day')
+# plt.ylabel('New Cases')
+# #plt.legend()
+# plt.show()	
 
-
-
-
-
-
-
-
-
-
+# plt.figure(4)
+# plt.plot(deaths_diff, 'r-')
+# plt.xlabel('Day')
+# plt.ylabel('New Deaths')
+# #plt.legend()
+# plt.show()	
 
